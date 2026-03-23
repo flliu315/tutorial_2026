743667ac128ca1297389bd6065093232e91230c9
flliu315 <flliu315@163.com>
2026-03-23 09:01:55 +0800

    adding two functions

---
+++ src/add_three.R
@@ -0,0 +1,6 @@
+## self-defined functions
+
+add_three <- function(x){
+  y <- x + 3
+  return(y)
+}
---
+++ src/myMean.R
@@ -0,0 +1,6 @@
+myMean <- function(x){
+  total_count_of_values <- length(x)
+  total_sum_of_values <- sum(x)
+  average_of_values <- total_sum_of_values/total_count_of_values
+  average_of_values
+}
